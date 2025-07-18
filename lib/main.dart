163660385794786f79b76151579cc1cba92c4303
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-07-18 18:02:04 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
+import 'package:animation_practice/Implicit_Animation/Animatedcontainer.dart';
 import 'package:flutter/material.dart';
+import 'Implicit_Animation/Animatedalign.dart';
 
-void main() => runApp(
-  MaterialApp(
-	home: Scaffold(
-	  appBar: AppBar(title: Text('Animation Practice')),
-	  body: Center(child: Text('Hello, Flutter!')),
-	),
-  ),
-);
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // ✨ Change this widget to test other animations
+  final Widget _currentExample = const AnimatedContainerExample();
+  // e.g. const AnimatedContainerExample();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Implicit Animations',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Implicit Animation Demo')),
+        body: Center(child: _currentExample),
+      ),
+    );
+  }
+}
  
  
