da8d0cf39534ee693be9b6ad3f94f17031ee599d
adityaphadatare45 <adityaphadatare4545@gmail.com>
2025-08-15 12:07:06 +0530

    Started physics animation

--- animation_practice/lib/Implicit_Animation/Animatedphysicalmodel.dart
+++ animation_practice/lib/Implicit_Animation/Animatedphysicalmodel.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 
+class Animatedphysicalmodel extends StatefulWidget{
+  const Animatedphysicalmodel({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+  
+}
